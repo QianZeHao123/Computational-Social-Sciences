be8aa25501c784757b7302bdf7b5f621fe38fcf6
Zehao Qian <qianzehao123@gmail.com>
2024-04-19 11:57:50 +0100

    update

---
+++ Summative/Code/CleanEnv.R
@@ -0,0 +1,11 @@
+# --------------------------------------------------------
+# clear the environment var area
+rm(list = ls())
+# clear all plots
+graphics.off()
+# clear the console area
+cat("\014")
+
+
+
+
